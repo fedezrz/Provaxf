61df1e9042d1ca76792befe74861c1c38431107d
Federico <fedezrz@gmail.com>
2017-06-14 17:36:51 +0200

    Lavoro di mercoledì pomeriggio 14/06/2017

--- quattro-input.php
+++ quattro-input.php
@@ -1,23 +1,13 @@
 <?php 
 	include("header.php");
 ?>
-	<div class="tabella-scrittura">	
-		<a name="cripta"></a>
-		      <div class="tabella-scrittura">
-		        <p class="inserisciFrase">Inserisci Frase:</p>
-		        <form  method="post" action="visualizza-quattro.php">
-		       		<div class="input-group">
-		       		  <span class="input-group-addon" id="basic-addon1">TESTO DA CRIPTARE:</span>
-		       		  <input name="primo" type="number" class="form-control" placeholder="Casuale" aria-describedby="basic-addon1">
-		       		  <input name="secondo" type="number" class="form-control" placeholder="Name" aria-describedby="basic-addon1">
-		       	      <input name="terzo" type="number" class="form-control" placeholder="Email" aria-describedby="basic-addon1">
-		       		  <input name="quarto" type="number" class="form-control" placeholder="Cellulare" aria-describedby="basic-addon1">
-		       		</div>
-		       		 <button type="submit" class="btn btn-default">Submit</button>
-		        </form>
-		      </div>
-
-	</div>	      
+	<form action="visualizza-quattro.php" method="post">
+		Cellulare: <input type="number" name="name"><br>
+		Numero conto bancario: <input type="number" name="email"><br>
+		La tua matricola è: <input type="number" name="matricola"><br>
+		Il tuo numero di scarpe è: <input type="number" name="scarpa">
+		<input type="submit">
+	</form>
 <?php 
 	include("footer.php");
 ?>
--- visualizza-quattro.php
+++ visualizza-quattro.php
@@ -1,4 +1,8 @@
-<?php 
-echo $POST["quattroVal"];
 
- ?>
+
+il tuo numero di cellulare è <?php echo $_POST["name"]; ?><br>
+il tuo numero della banca è: <?php echo $_POST["email"]; ?><br>
+la tua matricola è: <?php echo $_POST["matricola"]; ?><br>
+il tuo numero di scarpe è: <?php echo $_POST["scarpa"]; ?><br>
+
+Il numero più grande tra quelli inseriti è: <?php echo (max($_POST["name"], $_POST["email"], $_POST["matricola"], $_POST["scarpa"])); ?>
