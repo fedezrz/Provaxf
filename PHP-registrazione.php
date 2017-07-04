e1cc16cfdbd87c7365cde8cb55007277fb0ce519
Federico <fedezrz@gmail.com>
2017-07-04 17:28:39 +0200

    Lavoro di martedì 04/07/2017 parte conclusiva della tabella per gli utenti

--- PHP-registrazione.php
+++ PHP-registrazione.php
@@ -1,3 +1,7 @@
+
+<!--file che va a stampare tutti i dati inseriti dall'utente in un file esternodi nome "newfile.txt"-->
+
+
 <?php 						//richiamare un file al cui interno è presente l'header per non dover andare a riscriverlo 	ogni volta			
 	include("header.php");
 ?>
@@ -16,17 +20,37 @@ la data è:<?php
 $myfile = fopen("text/newfile.txt", "a") or die("Unable to open file!");
 
 
-
-
-
  $nome=$_POST["Nome"].",";
  $cognome=$_POST["Cognome"].",";
  $email=$_POST["Email"].",";
- $giorno=$_POST["giorno"]."\r\n";
+ $giorno=$_POST["giorno"].",";
+ $mese=$_POST["mese"].",";
+ $anno=$_POST["anno"]."\r\n";
 fwrite($myfile, $nome);
 fwrite($myfile, $cognome);
 fwrite($myfile, $email);
 fwrite($myfile, $giorno);
+fwrite($myfile, $mese);
+fwrite($myfile, $anno);
+
+
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "dbFederz";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$Data= $_POST["anno"]."/".$_POST["mese"]."/".$_POST["giorno"];
+$sql = "INSERT INTO users (Nome, Cognome, Mail, Data) VALUES ('".$_POST["Nome"]."', '".$_POST["Cognome"]."', '".$_POST["Email"]."', '".$Data."')";
+
+$result = $conn->query($sql);
 
 ?>
 
--- tabella-dati.php
+++ tabella-dati.php
@@ -1,3 +1,6 @@
+<!--file per creare una tabella contenente tutti i dati utente che vengono inseriti-->
+
+
 
 <?php 						//richiamare un file al cui interno è presente l'header per non dover andare a riscriverlo 	ogni volta			
 	include("header.php");
@@ -10,7 +13,7 @@ $handle = fopen($filename, "r");
 
 echo "<table>";
 	echo "<tr>";
-	echo "<td>"."Nome"."|"."</td>"."<td>"."Cognome"."|"."</td>"."<td>"."Email"."   "."|"."</td>"."<td>"."giorno"."|"."</td>";
+	echo "<td>"."Nome"."|"."</td>"."<td>"."Cognome"." |"."</td>"."<td>"."Email"."|"."</td>"."<td>"."giorno"."|"."</td>"."<td>"."mese"."|"."</td>"."<td>"."anno"."|"."</td>";
 	echo "</tr>";
 while (($line = fgets($handle,filesize($filename))) !== false){
 	$arrLine=explode(",", $line);
@@ -32,13 +35,55 @@ $lenFile=filesize("text/newfile.txt");
 $txt=file_get_contents ("text/newfile.txt",true);
 
 
+/*// hostname
+$nomehost = "localhost";   
+// utente per la connessione a MySQL  
+$nomeuser = "root";
+// password per l'autenticazione dell'utente 
+$password = "";
+// connessione tramite mysql_connect()
+$connessione = mysql_connect($nomehost,$nomeuser,$password);
+$selezione = mysql_select_db("dbFederz",$connessione) or die (mysql_error());
+$sqlTxt="SELECT * FROM users";
 
+ $sql = mysql_query($sqlTxt) or die (mysql_error());
+ $res=mysql_fetch_row($sql);
+var_dump($res);
+ foreach ($res as $key => $value) {
+ 	echo $value; 
+ }
+ mysql_close();*/
 
 
 
 ?><br>
 
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "dbFederz";
 
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT * FROM users";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["Id"]. " - Name: " . $row["Nome"]. " " . $row["Cognome"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
 
 
 <?php 						//richiamare un file al cui interno è presente l'header per non dover andare a riscriverlo 	ogni volta			
